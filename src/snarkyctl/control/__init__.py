5b86cb80e4b0db9d6eaf58eb857167fe686a0e5a
Top J. Quark <37160280+topquark22@users.noreply.github.com>
2026-07-22 21:20:03 -0400

    Add src/snarkyctl/control/__init__.py

---
+++ src/snarkyctl/control/__init__.py
@@ -0,0 +1,5 @@
+"""Privileged control-daemon protocol and server."""
+
+from snarkyctl.control.protocol import PROTOCOL_VERSION, ControlRequest, Operation
+
+__all__ = ["PROTOCOL_VERSION", "ControlRequest", "Operation"]
