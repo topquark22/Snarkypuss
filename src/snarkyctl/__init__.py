09d74fa1de4f02e8bf31276bd795c52930f69cf2
Geoffrey T. Falk <topquark22@yahoo.ca>
2026-07-26 00:51:34 -0400

    bump version to 1.0.0

--- src/snarkyctl/__init__.py
+++ src/snarkyctl/__init__.py
@@ -6,6 +6,6 @@ try:
     __version__ = version("snarkyctl")
 except PackageNotFoundError:  # pragma: no cover - only used before installation
     # Allows commands to run directly from an uninstalled source checkout.
-    __version__ = "0.10.0.dev4"
+    __version__ = "1.0.0"
 
 __all__ = ["__version__"]
